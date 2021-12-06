55d8bf728dc75092d3ccfebed2f01b80d6a72cfe
zhixiangyao <blueskyzx@icloud.com>
2021-12-06 18:25:58 +0800

    update

--- src/day-6/index.ts
+++ src/day-6/index.ts
@@ -5,7 +5,7 @@ const lines = readFileSync(__dirname, 'data.txt', ',')
 /**
  * part one
  */
-function partOne(lines: string[]): number {
+function partOne(lines: string[], maxDay = 80): number {
   class LanternFish {
     constructor(public timer = 8) {}
     next() {
@@ -19,7 +19,6 @@ function partOne(lines: string[]): number {
     }
   }
 
-  const maxDay = 80
   const fishes = lines.map(time => new LanternFish(Number(time)))
 
   let day = 0
@@ -38,9 +37,52 @@ function partOne(lines: string[]): number {
 /**
  * part two
  */
-function partTwo() {}
+function partTwo(lines: string[], maxDay = 256): number {
+  const fishList = lines.map(num => Number(num))
+
+  /**
+   * Map(5) {
+   *  3 => 32,
+   *  2 => 33,
+   *  1 => 175,
+   *  4 => 28,
+   *  5 => 32
+   * }
+   */
+  const HashFish = new Map<number, number>()
+
+  // fish 0 - 7
+  for (const fish of fishList) {
+    const existing = HashFish.get(fish)
+    if (existing) {
+      HashFish.set(fish, existing + 1)
+    } else {
+      HashFish.set(fish, 1)
+    }
+  }
+
+  let day = 0
+  while (day < maxDay) {
+    console.log(day % 9)
+    const previous = HashFish.get(day % 9) ?? 0
+
+    const key = (day + 7) % 9
+    const existing = HashFish.get(key)
+    if (existing) {
+      HashFish.set(key, existing + previous)
+    } else {
+      HashFish.set(key, previous)
+    }
+    day++
+  }
+
+  return [...HashFish.values()].reduce((acc, cur) => {
+    acc += cur
+    return acc
+  }, 0)
+}
 
 console.time('Time:')
 console.log('day-6-part-1:', partOne(lines))
-console.log('day-6-part-2:', partTwo())
+console.log('day-6-part-2:', partTwo(lines))
 console.timeEnd('Time:')
